eacc02d8a65d76e28590930a2a9589201870dbc5
Cloud User <ec2-user@ip-172-31-19-88.ap-southeast-2.compute.internal>
2016-08-08 03:31:12 +1000

    Add mongo search for query and sample index as root

--- search/app.py
+++ search/app.py
@@ -2,12 +2,33 @@
 from flask import Flask, jsonify
 from flask import request
 
-app = Flask(__name__)
+from flask_pymongo import PyMongo
+import json
+from bson import json_util
+
+
+app = Flask(__name__, static_url_path='')
+
+# configuration
+app.config['MONGO_HOST'] = 'localhost'
+app.config['MONGO_PORT'] = 27017
+app.config['MONGO_DBNAME'] = 'items'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('query', '')
-    return jsonify([{'text': "Hello, World!", 'author': 'Name'}, {'query': query}])
+    data = mongo.db.article_items.find({'$text': {'$search': query}} , {'_id': 0, 'text': 0, 'score': {'$meta': "textScore"}}).sort([('score', {'$meta': "textScore"})])
+    #res = json.dumps(data, default=json_util.default)
+    res = [r for r in data]
+    for r in res:
+        del r['score']
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
